b8a1e6272b3d541ecf5e5049af0396367b43b119
Macbook Air <macbookair@Obinnas-MacBook-Air.local>
2025-01-04 09:57:32 +0100

    Home Page

--- src/app/page.js
+++ src/app/page.js
@@ -5,16 +5,15 @@ import AnimatedText from "./Components/AnimatedText";
 
 export default function Home() {
   return (
-    <div className="space-y-12">
+    <div className="space-y-4 md:space-y-12">
       <HeroSection />
 
       <hr className=""  />
 
-      <div className=" mt-8 w-[90%] flex flex-col md:flex-row gap-8 md:gap-12 m-auto ">
-        <div className="md:w-1/2 flex flex-col gap-6 ">
-        <Typography className="text-left text-xs md:text-sm   ">
-          <span className="text-[#EBB709]">D'Square</span> event Centre Located in the heart of Idimu,
-          Ikotun, D'Square Event Centre is where unforgettable celebrations come
+      <div className="w-[90%] flex flex-col  md:flex-row gap-8 md:gap-12 m-auto ">
+        <div className="md:w-1/2 flex flex-col gap-12  ">
+        <Typography className="text-left text-xs lg:text-sm leading-relaxed   ">
+          Located in the heart of Idimu, Ikotun, <span className="text-[#EBB709] font-semibold">D'Square</span>  Event Centre is where unforgettable celebrations come
           to life. With our spacious, elegant halls and modern facilities, we
           cater to all your event needs, from weddings and birthdays to
           corporate gatherings and more. At D'Square, we believe in making every
@@ -24,16 +23,15 @@ export default function Home() {
           and experience celebration redefined.
         </Typography>
 
-        <Typography className="w-full text-left text-xs md:text-sm  ">
+        <Typography className="w-full text-left text-xs lg:text-sm leading-relaxed  ">
            At D'Square, we believe in making every
           moment extraordinary. That's why our motto is: "Think Celebration,
           Think D'Square." Let us help you create memories that last a lifetime.
-          Explore our venue and services today! Contact us to book your event
-          and experience celebration redefined.
+          Explore our venue and services today! 
         </Typography>
 
         
-      <Button className="mt-20 px-6 md:px-4 py-3 md:text-lg self-start bg-[#EBB709] text-black hover:scale-110 text-sm">More about us</Button>
+      <Button className=" px-4 py-3 self-start bg-[#EBB709] text-black hover:scale-110 text-xs rounded-full font-semibold">More about us</Button>
         </div>
         
 
@@ -43,9 +41,9 @@ export default function Home() {
           <Image src="/Gallery/HeroImage3.jpeg"
           alt=""
          
-               width={500}
-               height={500}
-                className="object-cover "  />
+               width={800}
+               height={200}
+                className="max-h-[350px] md:max-h-[400px]"  />
         </div>
       
       </div>
